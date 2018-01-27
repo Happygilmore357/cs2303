00acf18922a8d44bb00f2a31f62f1d835f2973f8
isaiahfleischer@gmail.com <isaiahfleischer@gmail.com>
2018-01-27 14:54:37 -0500

    1/2 done

---
+++ PA2_idfleischer/PlayOne.c
@@ -0,0 +1,27 @@
+/*
+ * PlayOne.c
+ *
+ *  Created on: Jan 25, 2018
+ *      Author: isaiah
+ */
+
+#include "life.h"
+
+/** Play 1 turn of Conway's Game of Life
+ *
+ * @param rows Number of rows
+ * @param columns Number of columns
+ * @param old Board from previous turn
+ * @param new New board
+ * @return 1 if the game has repeated itself.
+ * 	  in the past 2 turns. 0 if board hasn't.
+ * 	  repeated itself.
+ */
+int PlayOne(unsigned int rows, unsigned int columns, int **Old, int **New) {
+
+
+
+	return 0;
+}
+
+
---
+++ PA2_idfleischer/life.h
@@ -0,0 +1,16 @@
+/*
+ * life.h
+ *
+ *  Created on: Jan 27, 2018
+ *      Author: isaiah
+ */
+
+
+#ifndef LIFE_H_
+#define LIFE_H_
+
+int PlayOne(unsigned int rows, unsigned int columns, int **Old, int **New);
+
+
+
+#endif /* LIFE_H_ */
